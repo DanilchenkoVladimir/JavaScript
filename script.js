344efd3c937bad19f25ffbf8741c74151a601925
Vova <ppgpilot@yandex.ru>
2022-02-04 22:33:46 +0200

    дз сделано

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 // Task 1//
 
-function calc(operator, value1, value2) {
-    if (operator === '+') {
-        return value1 + value2
-    }
-    else if (operator === '-') {
-        return value1 - value2
-    }
-    else if (operator === '*') {
-        return value1 * value2
-    }
-    else if (operator === '/') {
-        return value1 / value2
-    }
+function removeChar(word){
+    newWord = word.slice(1, word.length -1)
+    return newWord
 }
 
-console.log (calc('+', 4, 7))
-console.log (calc('-', 15, 18))
-console.log (calc('*', 5, 5))
-console.log (calc('/', 49, 7))
+console.log(removeChar('eloquent')); // loquen
+console.log(removeChar('country')); // ountr
+console.log(removeChar('person')); // erso
+
+
+
 
 //Task 2//
 
-function squareOfSum(number1, number2){
+const coll1 = [8, 9, 21, 19, 18, 22, 7]; 
+const coll2 = [2, 0, 17, 3, 9, 15, 4]; 
 
-    return (number1 ** 2) + (( 2 * number1) * number2) + (number2 ** 2)
-  
+function calculateSum(items){
+    let sum = 0;
+    items.forEach(function(item){
+        if (item % 3 === 0) {
+            sum = sum + item
+        }
+    })
+    return sum
 }
 
-console.log(squareOfSum(2,3))
-console.log(squareOfSum(1,10))
-
-//Task 3//
-
-function finalGrade(exam, projects) {
-
-    if (exam >= 90, projects >= 10) {
-        return 'your rating = 100'
-    }
-    else if (exam >= 75, projects >= 5) {
-        return 'your rating = 90'
-    }
-    else if (exam >= 50, projects >= 2) {
-        return 'your rating = 75'
-    }
-    else if (exam < 50, projects < 2) {
-        return 'your rating = 0'
-    }
-}
-
-console.log (finalGrade(90, 10))
+console.log(calculateSum(coll1)) // 48 
+console.log(calculateSum(coll2)) // 27
