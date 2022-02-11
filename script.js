07ceaf58934e0b835bfe5a1becebcbf84d13b9c4
Vova <ppgpilot@yandex.ru>
2022-02-11 23:12:19 +0200

    дз сделано

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
 // Task 1//
 
-function calc(operator, value1, value2) {
-    if (operator === '+') {
-        return value1 + value2
-    }
-    else if (operator === '-') {
-        return value1 - value2
-    }
-    else if (operator === '*') {
-        return value1 * value2
-    }
-    else if (operator === '/') {
-        return value1 / value2
-    }
+const names = ['john', 'smith', 'karl'];
+let newArr= [];
+
+function reverse(item){
+    names.forEach(function(item){
+        newArr.unshift(item)
+    })
+    return newArr;
 }
 
-console.log (calc('+', 4, 7))
-console.log (calc('-', 15, 18))
-console.log (calc('*', 5, 5))
-console.log (calc('/', 49, 7))
+console.log(reverse(newArr)); // [ 'karl', 'smith', 'john' ]
 
 //Task 2//
 
-function squareOfSum(number1, number2){
-
-    return (number1 ** 2) + (( 2 * number1) * number2) + (number2 ** 2)
-  
+function pangrams(item) {
+    const letters = "abcdefghijklmnopqrstuvwxyz"; 
+    for(let i = 0; i < letters.length; i++){
+      if(item.indexOf(letters[i]) === -1){
+        return "false";
+      }
+    }
+    return "true";
 }
 
-console.log(squareOfSum(2,3))
-console.log(squareOfSum(1,10))
-
-//Task 3//
-
-function finalGrade(exam, projects) {
-
-    if (exam >= 90, projects >= 10) {
-        return 'your rating = 100'
-    }
-    else if (exam >= 75, projects >= 5) {
-        return 'your rating = 90'
-    }
-    else if (exam >= 50, projects >= 2) {
-        return 'your rating = 75'
-    }
-    else if (exam < 50, projects < 2) {
-        return 'your rating = 0'
-    }
-}
-
-console.log (finalGrade(90, 10))
+console.log(pangrams('The quick brown fox jumps over the lazy dog')); //true
+console.log(pangrams('We promptly judged antique ivory buckles for the prize')); //false
