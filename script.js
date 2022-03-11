1af99869625c6f001f4ad037b93a388f976114d9
danila <ppgpilot@yandex.ru>
2022-03-12 01:13:58 +0200

    ok

--- script.js
+++ script.js
@@ -1,52 +1,26 @@
 // Task 1//
 
-function calc(operator, value1, value2) {
-    if (operator === '+') {
-        return value1 + value2
-    }
-    else if (operator === '-') {
-        return value1 - value2
-    }
-    else if (operator === '*') {
-        return value1 * value2
-    }
-    else if (operator === '/') {
-        return value1 / value2
-    }
+function toJadenCase(phrase){    
+    return phrase.split(' ').map(word => word.slice(0,1).toUpperCase() + word.slice(1)).join(' '); 
 }
 
-console.log (calc('+', 4, 7))
-console.log (calc('-', 15, 18))
-console.log (calc('*', 5, 5))
-console.log (calc('/', 49, 7))
+console.log(toJadenCase('How can mirrors be real if our eyes aren"t real')); // How Can Mirrors Be Real If Our Eyes Aren"t Real
 
 //Task 2//
 
-function squareOfSum(number1, number2){
-
-    return (number1 ** 2) + (( 2 * number1) * number2) + (number2 ** 2)
-  
+function oddOrEven(items){
+    let sum = 0;
+    items.forEach(function(item){
+        sum = sum + item;        
+    })
+    if (sum % 2 == 0){
+        return "even";
+    } else return "odd";    
 }
 
-console.log(squareOfSum(2,3))
-console.log(squareOfSum(1,10))
-
-//Task 3//
-
-function finalGrade(exam, projects) {
-
-    if (exam >= 90, projects >= 10) {
-        return 'your rating = 100'
-    }
-    else if (exam >= 75, projects >= 5) {
-        return 'your rating = 90'
-    }
-    else if (exam >= 50, projects >= 2) {
-        return 'your rating = 75'
-    }
-    else if (exam < 50, projects < 2) {
-        return 'your rating = 0'
-    }
-}
-
-console.log (finalGrade(90, 10))
+console.log(oddOrEven([0, 1, 5])); // 'even' 'четное'
+console.log(oddOrEven([0, 1, 3])); // 'even'
+console.log(oddOrEven([0, -1, -5])); // 'even'
+console.log(oddOrEven([0, 1, 4])); // 'odd' 'нечетное'
+console.log(oddOrEven([0, -1, 2])); // 'odd'
+console.log(oddOrEven([0])); // 'even'
